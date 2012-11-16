bf3c629dd5fbf405fdb0e0634f8a5edfad02285c
Sam Saccone <samccone@gmail.com>
2012-11-15 22:36:59 -0500

    fix dumb mistake

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ exports.getWeather = function(lat, lng, cb) {
   var apiUrl = "http://forecast.weather.gov/MapClick.php?lat="+lat+"&lon="+lng+"&site=okx&unit=0&lg=en&FcstType=json";
   request.get(apiUrl, function(err, body, res) {
     cb(err, JSON.parse(res));
-  }
+  });
 }
