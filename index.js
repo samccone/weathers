c33d61bc777edc41629455ce614479fdcc3fe7cd
Sam Saccone <samccone@gmail.com>
2013-10-01 14:24:26 -0400

    handle case when the JSON parsing could fail

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var request = require('request');
 exports.getWeather = function(lat, lng, cb) {
   var apiUrl = "http://forecast.weather.gov/MapClick.php?lat="+lat+"&lon="+lng+"&site=okx&unit=0&lg=en&FcstType=json";
   request.get(apiUrl, function(err, body, res) {
-    cb(err, JSON.parse(res));
+    try {
+      cb(err, JSON.parse(res));
+    } catch (e) {
+      cb(new Error("Error Parsing Response as JSON"), null);
+    }
   });
 }
